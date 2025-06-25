507a7a70b1a45d59a6054478839483a5a55b40f0
LilFoDev <danifortvideogamedeveloper@gmail.com>
2025-06-25 21:14:18 +0200

    app working, next step: customize page

--- APP/streamlit_app.py
+++ APP/streamlit_app.py
@@ -7,21 +7,29 @@ from PIL import Image
 from ui.custom_elements.running_layout import RunningLayout
 from vision.web_cam_reader import WebCamReader
 from utils.image_flow import get_frame_info,update_frame
-
+from utils import time_control
 
 st.title('HANDS TALK')
 
 class VideoTransformer(VideoTransformerBase):
     def __init__(self):
+        self.time_controller = time_control.FrameTimer()
         self.wc = WebCamReader(is_web = True)
         self.lout = RunningLayout()
+        self.components_started = False
 
     def transform(self,frame):
+        self.time_controller.should_update()
         img = frame.to_ndarray(format='bgr24')
         update_frame(img)
+        if not self.components_started:
+            self.wc.start()
+            self.lout.start()
+            self.components_started = True
+
         self.wc.update()
-        # self.lout.update()
-        # self.lout.draw()
+        self.lout.update()
+        self.lout.draw()
 
         return img
 
