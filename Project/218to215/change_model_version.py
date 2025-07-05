db2a647c4c554a4531f2f8d34622d557ed12f596
LilFoDev <danifortvideogamedeveloper@gmail.com>
2025-07-05 16:43:44 +0200

    a

--- Project/218to215/change_model_version.py
+++
@@ -1,9 +0,0 @@
-from tensorflow import keras
-org_model_path = 'APP/Files/models/ASP_3_model_7.keras'
-dst_model_path = 'APP/Files/models/ASP_3_model_7_v215.h5'
-
-model = keras.models.load_model(org_model_path)
-print('Model loaded successfuly')
-
-
-print('Model saved successfuly')
